5c786b7342cefd26907569dadf66aee5a31354fe
Snorre Eskeland Brekke <snorre.e.brekke@gmail.com>
2022-10-07 12:10:36 +0200

    chore: replaced parcel with pkg-utils for building

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import {createPlugin} from 'sanity'
-import {codeSchema, CodeDefinition} from './schema'
 
+import {codeSchema, codeTypeName, CodeDefinition} from './schema'
 import PreviewCode, {PreviewCodeProps} from './PreviewCode'
 export type {CodeInputProps, CodeSchemaType, CodeOptions} from './CodeInput'
+
 export type {CodeInputLanguage, CodeInputValue} from './types'
 
 export {PreviewCode, type PreviewCodeProps}
-export {codeSchema}
+export {codeSchema, codeTypeName}
 export type {CodeDefinition}
 /**
  * @public
