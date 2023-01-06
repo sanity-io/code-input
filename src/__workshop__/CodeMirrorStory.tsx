579bf739b152e615434dc314cf7bc9e52ee38e5e
Snorre Eskeland Brekke <snorre.e.brekke@gmail.com>
2023-01-06 10:04:04 +0100

    chore: story select

--- src/__workshop__/CodeMirrorStory.tsx
+++ src/__workshop__/CodeMirrorStory.tsx
@@ -1,18 +1,23 @@
 import {Card, Container} from '@sanity/ui'
-import {useString} from '@sanity/ui-workshop'
 import React, {Suspense, useState} from 'react'
 import {useCodeMirror} from '../codemirror/useCodeMirror'
+import {SUPPORTED_LANGUAGES} from '../config'
+import {useSelect} from '@sanity/ui-workshop'
+
+const langs = {
+  ...Object.fromEntries(SUPPORTED_LANGUAGES.map((l) => [l.title, l.mode ?? l.value])),
+  'Not supported': 'not-supported',
+}
+
+const defaultSnippet = `
+<Card border padding={2}>
+ {variable}
+</Card>
+`
 
 export default function CodeMirrorStory() {
-  const language = useString('Language mode', 'tsx')
-
-  // this does not currently work (react update depth error)
-  /*  const language = useSelect(
-    'Language mode',
-    Object.fromEntries(SUPPORTED_LANGUAGES.map((l) => [l.mode ?? l.value])),
-    'text'
-  )*/
-  const [code, setCode] = useState('')
+  const language = useSelect('Language mode', langs, 'tsx')
+  const [code, setCode] = useState(defaultSnippet)
   const [highlights, setHighlights] = useState<number[]>([])
 
   const CodeMirror = useCodeMirror()
