5b751e92c2079b2e69f8f9f639cdb7cd98cfa990
Simeon Griggs <simeon@hey.com>
2023-02-24 16:54:25 +0000

    feat: richer preview component

--- src/PreviewCode.tsx
+++ src/PreviewCode.tsx
@@ -1,6 +1,6 @@
 import {Suspense} from 'react'
 import styled from 'styled-components'
-import {Box, Card} from '@sanity/ui'
+import {Label, Box, Card, Flex, Text} from '@sanity/ui'
 import {CodeInputValue, CodeSchemaType} from './types'
 import {PreviewProps} from 'sanity'
 import {useCodeMirror} from './codemirror/useCodeMirror'
@@ -28,12 +28,31 @@ export default function PreviewCode(props: PreviewCodeProps) {
   return (
     <PreviewContainer>
       <Card padding={4}>
+        {selection?.filename || selection?.language ? (
+          <Card
+            paddingBottom={4}
+            marginBottom={selection.code ? 4 : 0}
+            borderBottom={!!selection.code}
+          >
+            <Flex align="center" justify="flex-end">
+              {selection?.filename ? (
+                <Box flex={1}>
+                  <Text>
+                    <code>{selection.filename}</code>
+                  </Text>
+                </Box>
+              ) : null}
+              {selection?.language ? <Label muted>{selection.language}</Label> : null}
+            </Flex>
+          </Card>
+        ) : null}
         {CodeMirror && (
           <Suspense fallback={<Card padding={2}>Loading code preview...</Card>}>
             <CodeMirror
               readOnly
               editable={false}
               value={selection?.code || ''}
+              highlightLines={selection?.highlightedLines || []}
               basicSetup={{
                 lineNumbers: false,
                 foldGutter: false,
