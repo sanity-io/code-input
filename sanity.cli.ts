add179468b5d4dec3a94a6702088c69953270dcb
Cody Olsen <stipsan@gmail.com>
2025-10-15 19:58:26 +0200

    chore: set appId

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -2,6 +2,6 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {projectId: 'ppsg7ml5', dataset: 'test'},
-  autoUpdates: true,
+  deployment: {appId: 'g2def28kqacghk3oy8ezpte1', autoUpdates: true},
   reactStrictMode: true,
 })
